b057edbf81cfe367ebe68cabc46f9077531c2246
DhanushaNandakumar <103499076+DhanushaNandakumar@users.noreply.github.com>
2022-05-03 12:17:13 +0530

    Add files via upload

---
+++ API/src/auth/passport.js
@@ -0,0 +1,22 @@
+const passport = require("passport");
+const passportJwt = require("passport-jwt");
+const ExtractJwt = require("passport-Jwt").ExtractJwt;
+const StrategyJwt = require("passport-Jwt").Strategy;
+const User = require("../models/user");
+passport.use(
+  new StrategyJwt(
+    {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET,
+    },
+    function (jwtPayload, done) {
+      return User.findOne({ where: { id: jwtPayload.id } })
+        .then((user) => {
+          return done(null, user);
+        })
+        .catch((err) => {
+          return done(err);
+        });
+    }
+  )
+);
